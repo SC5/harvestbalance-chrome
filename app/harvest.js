4811c6317e9e6cb8ddcb6ba63b96d5329f0cd9df
Arto Liukkonen <arto@51.fi>
2017-05-10 14:10:02 +0300

    Change default kiky hour requirement to 3*7.5. Bump version.

--- app/harvest.js
+++ app/harvest.js
@@ -17670,7 +17670,7 @@ return hooks;
 const namespace = "harvestBalance.hours.";
 const kikyNamespace = "harvestBalance.kiky.";
 const kikyTask = 7115157; // Task id to collect KiKy hours
-const kikyHours = 24; // Yearly required KiKy hours
+const kikyHours = 22.5; // Yearly required KiKy hours
 
 module.exports = {
   namespace,
